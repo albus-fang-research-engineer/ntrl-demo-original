634505798d1b8ea4e233d905c8b6fcff4e9e2420
Albus Fang <albus.fang.research.engineer@gmail.com>
2026-01-04 16:42:29 -0500

    added box/

---
+++ datasets/test/box/plot_sampled_points.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the file
+points = np.load("sampled_points.npy")     # <-- change filename
+
+print(points.shape)                # sanity-check (should be N x 2)
+
+# plot
+plt.figure(figsize=(6,6))
+plt.scatter(points[:,0], points[:,1], s=0.05)
+plt.axis('equal')
+plt.title("Sampled 2D Points")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig("sampled points.png")
+plt.show()
