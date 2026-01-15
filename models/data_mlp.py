ce291b9b4fe3a495253f46b70e5bd5ffd8d5c31d
Albus Fang <albus.fang.research.engineer@gmail.com>
2026-01-14 22:23:16 -0500

    added normal mean and var loading capabilities in data_mlp.py

--- models/data_mlp.py
+++ models/data_mlp.py
@@ -5,15 +5,16 @@ from torch.autograd import Variable, grad
 #import open3d as o3d
 
 class _numpy2dataset(torch.utils.data.Dataset):
-    def __init__(self, points, speed, speed_var, normal):
+    def __init__(self, points, speed, speed_var, normal, normal_euclidean_var, normal_angular_var):
         # Creating identical pairs
         points    = Variable(Tensor(points))
         speed  = Variable(Tensor(speed))
         speed_var  = torch.as_tensor(speed_var,  dtype=torch.float32)
         normal  = Variable(Tensor(normal))
-        self.data=torch.cat((points,speed,speed_var,normal),dim=1)
+        self.data=torch.cat((points,speed,speed_var,normal, normal_euclidean_var, normal_angular_var),dim=1)
         #self.grid  = Variable(Tensor(grid))
-
+        normal_euclidean_var = torch.as_tensor(normal_euclidean_var, dtype=torch.float32)
+        normal_angular_var   = torch.as_tensor(normal_angular_var,   dtype=torch.float32)
     def send_device(self,device):
         self.data    = self.data.to(device)
 
@@ -31,6 +32,8 @@ def Database(PATH):
     speed = np.load('{}/speed.npy'.format(PATH))#[:100000,:]
     speed_var  = np.load(f"{PATH}/speed_var.npy")
     normal = np.load('{}/normal.npy'.format(PATH))#[:100000,:]
+    normal_euclidean_var = np.load('{}/normal_euclidean_var.npy'.format(PATH))
+    normal_angular_var = np.load('{}/normal_ang_var.npy'.format(PATH))
     #occupancies = np.unpackbits(np.load('{}/voxelized_point_cloud_128res_20000points.npz'.format(PATH))['compressed_occupancies'])
     #input = np.reshape(occupancies, (128,)*3)
     #grid = np.array(input, dtype=np.float32)
@@ -55,7 +58,7 @@ def Database(PATH):
     print(points.shape,speed.shape)
     #print(np.shape(grid))
     #print(XP.shape,YP.shape)
-    database = _numpy2dataset(points,speed,speed_var,normal)
+    database = _numpy2dataset(points,speed,speed_var,normal, normal_euclidean_var, normal_angular_var)
     #database = _numpy2dataset(XP,YP)
     return database
 
